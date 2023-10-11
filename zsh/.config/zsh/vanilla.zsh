13dbf4ea2916556a1151c5882e155cf7ceca1e78
whatlas <whatlas@qq.com>
2023-10-11 06:08:01 -0400

    minor fix

--- zsh/.config/zsh/vanilla.zsh
+++ zsh/.config/zsh/vanilla.zsh
@@ -148,7 +148,7 @@ mc() {
 }
 
 hostip() {
-    export HOST_IP="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1)"
+    export HOST_IP="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
     echo $HOST_IP
 }
 

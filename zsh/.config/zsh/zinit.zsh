2843ccb7fe8a8c9202f337c54a8c9de238964e05
whatlas <whatlas@qq.com>
2023-11-21 21:54:57 -0500

    update arch

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -10,8 +10,8 @@ if [ ! -f "$ZINIT_PATH/zinit.zsh" ]; then
 	echo "Installing zinit ..."
 	[ ! -d "$ZINIT_PATH" ] && mkdir -p "$ZINIT" 2> /dev/null
 	if [ -x "$(which git)" ]; then
-		setpx
-		git clone https://github.com/zdharma/zinit.git $ZINIT_PATH
+		# setpx
+		git clone https://github.com/zdharma-continuum/zinit.git $ZINIT_PATH
 	else
 		echo "ERROR: please install git before installation !!"
 		exit 1
@@ -37,7 +37,11 @@ zinit light jocelynmallon/zshmarks
 zinit light mafredri/zsh-async
 # zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-syntax-highlighting
+# zinit light zsh-users/zsh-syntax-highlighting
+zinit light z-shell/F-Sy-H
+
+zinit ice as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX"
+zinit light tj/git-extras
 
 # If completion is needed, de-comment lines below.
 # this will add 200ms loading time.
@@ -51,15 +55,15 @@ for dump in ~/.zcompdump(N.mh+24); do
 done
 compinit -C
 
-# ==================================================================
 # update zinit, only run update or first install
-# ==================================================================
 # zinit self-update
-
-# ==================================================================
-# plugin config
-# ==================================================================
 
 # ZSH_AUTOSUGGEST
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-ZSH_HIGHLIGHT_STYLES[comment]=fg=245
+# ZSH_HIGHLIGHT_STYLES[comment]=fg=245
+
+# put these 2 lines at the end of plugins settings
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# --- }}}

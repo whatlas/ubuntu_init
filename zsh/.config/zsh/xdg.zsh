feeced150653e12c495bca5d861c7e1b91183ba8
whatlas <whatlas@qq.com>
2024-09-24 03:59:19 -0400

    minor update

--- zsh/.config/zsh/xdg.zsh
+++ zsh/.config/zsh/xdg.zsh
@@ -1,16 +1,21 @@
 mkdir_if_not_exist() {
-    [[ -d "$1" ]] || mkdir -p "$1"
+    if [[ -d "$1" ]]; then
+        return
+    fi
+    mkdir -p "$1"
 }
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.local/state"
+
 mkdir_if_not_exist "$XDG_CONFIG_HOME"
 mkdir_if_not_exist "$XDG_DATA_HOME"
 mkdir_if_not_exist "$XDG_CACHE_HOME"
 mkdir_if_not_exist "$XDG_CACHE_HOME/zsh"
 mkdir_if_not_exist "$XDG_STATE_HOME"
+mkdir_if_not_exist "$XDG_STATE_HOME/zsh"
 
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 export CONAN_USER_HOME="$XDG_CONFIG_HOME"
